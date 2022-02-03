4208aaccf312cdc6a6251d70c4cdeeeeb4f71981
wzd200 <william.z.dye@gmail.com>
2022-02-03 17:57:07 -0500

    wip displaying movie info from axios call

--- movie-api/src/Components/Conditions/MovieConditions.js
+++ movie-api/src/Components/Conditions/MovieConditions.js
@@ -5,19 +5,19 @@ const StyledError = styled.div`
     color: crimson;
 `
 const MovieConditions = (props) => {
-
-    // console.log(props.responseObj)
+    // if (props.responseObj.Response === "True") {
+    //     const movieArray = props.responseObj.Search
+    // }
 
     return (
         <div>
-            {/* {props.responseObj && <div><p>{props.responseObj.response}</p></div>} */}
-            {/* <p>{props.responseObj.Search}</p> */}
             {props.error && <StyledError>Please enter a valid movie title</StyledError>}
             {props.loading && <div>loading...</div>}
             {/* {props.responseObj.Response === "True" ?
-                <p>response worked</p>
-                : <p>response didn't work</p>
-        } */}
+                movieArray.forEach(movie => {
+                    return <p>{movie.Title}</p>
+                }) : null
+            } */}
         </div>
     )
 }
